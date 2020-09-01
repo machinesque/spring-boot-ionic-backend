b26f08abf3013ed0c99f344a0d87a583daf5db69
Luiz Antonio Machinesque <machinesque@gmail.com>
2020-09-01 10:15:42 -0300

    Add Database h2 for tests and auto create database

--- src/main/java/com/udemy/cursomc/domain/Categoria.java
+++ src/main/java/com/udemy/cursomc/domain/Categoria.java
@@ -2,10 +2,15 @@ package com.udemy.cursomc.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.*;
+
+@Entity
 public class Categoria implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
 	private String nome;
