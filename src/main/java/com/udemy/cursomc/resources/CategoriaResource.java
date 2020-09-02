bacfca01bfe9a6ad5a46ff4057168fe9045ed48a
Luiz Antonio Machinesque <machinesque@gmail.com>
2020-09-02 11:41:02 -0300

    Creating Repository and Service for Categoria, and Create General Exception for Domain

--- src/main/java/com/udemy/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/udemy/cursomc/resources/CategoriaResource.java
@@ -3,35 +3,31 @@ package com.udemy.cursomc.resources;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.udemy.cursomc.domain.Categoria;
+import com.udemy.cursomc.services.CategoriaService;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
-	@GetMapping
-//	@RequestMapping(method = RequestMethod.GET)
-	public List<Categoria> listar() {
+	@Autowired
+	private CategoriaService categoriaService;
+	
+	
+	@GetMapping(value = "/{id}")
+//	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<?> findById(@PathVariable Long id) {
 		
-		Categoria categoria1 = new Categoria();
-		Categoria categoria2 = new Categoria();
+		Categoria categoria = categoriaService.buscarPorId(id);
+		return ResponseEntity.ok().body(categoria);
 		
-		categoria1.setId(1L);
-		categoria1.setNome("Informática");
-		
-		categoria2.setId(2L);
-		categoria2.setNome("Escritório");
-		
-		List<Categoria> listaCategorias = new ArrayList<>();
-
-		listaCategorias.add(categoria1);
-		listaCategorias.add(categoria2);
-		
-		return listaCategorias;
 	}
 	
 }
---
+++ src/main/java/com/udemy/cursomc/services/CategoriaService.java
@@ -0,0 +1,26 @@
+package com.udemy.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.udemy.cursomc.domain.Categoria;
+import com.udemy.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	public Categoria buscarPorId(Long id) {
+		Optional<Categoria> objetoCategoria = categoriaRepository.findById(id);
+		return objetoCategoria.orElse(null);
+		
+//		return categoriaRepository.findById(id)
+//				.orElseThrow(() -> new EntidadeNaoEncontradaException("Categoria não encontrada!"));
+		
+	}
+	
+}
